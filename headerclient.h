0627483851870c67b2da1304d07615347c8fd9aa
oooJordan <158740261+oooJordan@users.noreply.github.com>
2024-07-10 15:41:43 +0200

    Create headerclient.h

---
+++ headerclient.h
@@ -0,0 +1,26 @@
+#ifndef CLIENT_HEADER_H
+#define CLIENT_HEADER_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <libgen.h> //per usare la funzione basename()
+#include <linux/limits.h>
+#include <stdbool.h>
+#include <sys/stat.h>
+
+#define BUFFER_SIZE 4096
+
+int create_connection(const char *server_address, int server_port);
+void upload_file_on_server(int socket_file_descriptor, char *o_path, char *f_path);
+void download_file_from_server(int socket_file_descriptor, char *o_path, char *f_path);
+FILE* open_file(const char *path, const char *mode);
+void send_command(int socket_file_descriptor, char command, char* path_file_remoto);
+void file_list_on_stout(int socket_file_descriptor, char *f_path);
+void create_dir(const char *path);
+
+#endif
