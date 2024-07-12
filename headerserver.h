7bd5edc5e844c4fa42a206134c8ede076e68380b
oooJordan <158740261+oooJordan@users.noreply.github.com>
2024-07-12 09:47:33 +0200

    Update headerserver.h

--- headerserver.h
+++ headerserver.h
@@ -40,10 +40,7 @@ FileLock *file_locks = NULL;
 // Prototipi di funzione
 void *thread_function(void *arg);
 void command_client(int client_socket);
-FileLock *get_file_lock(const char *file);
-void release_file_lock(FileLock *lock);
-void lockfile(const char *file);
-void unlockfile(const char *file);
+
 int create_dir(const char *ft_root_directory);
 
 void rimuovi_accapo(char *stringa);
@@ -53,4 +50,11 @@ int function_for_download(int client_socket, char *path);
 void function_to_send_file(int client_socket, char *path);
 char *extract_directory_path(char *file_path);
 bool check_dir(char *path);
+
+
+//lock
+FileLock *get_file_lock(const char *file);
+void release_file_lock(FileLock *lock);
+void lockfile(const char *file);
+void unlockfile(const char *file);
 #endif
