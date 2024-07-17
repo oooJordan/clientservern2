3f9305b66702af71e50a636b2cd6bda93a734f71
oooJordan <158740261+oooJordan@users.noreply.github.com>
2024-07-17 11:39:21 +0200

    Update headerserver.h

--- headerserver.h
+++ headerserver.h
@@ -16,19 +16,18 @@
 #include <libgen.h>
 #include <semaphore.h>
 #include <stdint.h>
-#include <linux/limits.h>
 #include <stdbool.h>
 
 
-#define MAX_CLIENTS 10 // Massimo numero di client che possono essere gestiti contemporaneamente
-#define BUFFER_SIZE 4096 // Dimensione del buffer
+#define MAX_CLIENTS 4 // massimo numero di client che possono essere gestiti contemporaneamente
+#define BUFFER_SIZE 4096 // dimensione del buffer
 
-// Struct che rappresenta un lock associato a un file
+// struct che rappresenta un lock associato a un file
 typedef struct FileLock {
-    char file[1024]; // Nome del file
-    pthread_mutex_t lock; // Lock sul file
-    int ref_count; // Conteggio di quanti thread utilizzano il lock
-    struct FileLock *next; // Puntatore al prossimo FileLock (lista concatenata)
+    char file[1024]; // nome del file
+    pthread_mutex_t lock; // lock sul file
+    int count_lock; // conteggio di quanti thread utilizzano il lock su un determinato file
+    struct FileLock *next; // puntatore al prossimo FileLock (lista concatenata)
 } FileLock;
 
 // Variabili globali
@@ -36,21 +35,17 @@ int socket_file_descriptor;
 pthread_mutex_t file_locks_mutex = PTHREAD_MUTEX_INITIALIZER;
 FileLock *file_locks = NULL;
 
-
-// Prototipi di funzione
+// prototipi di funzione
 void *thread_function(void *arg);
 void command_client(int client_socket);
 
-int create_dir(const char *ft_root_directory);
+int create_dir(const char *percorso_directory);
 
 void rimuovi_accapo(char *stringa);
 bool check_path(char* path);
-int function_for_upload(int client_socket, char *path);
-int function_for_download(int client_socket, char *path);
-void function_to_send_file(int client_socket, char *path);
-char *extract_directory_path(char *file_path);
-bool check_dir(char *path);
-
+int function_for_upload_w(int client_socket, char *path);
+int function_for_download_r(int client_socket, char *path);
+void function_to_send_file_l(int client_socket, char *path);
 
 //lock
 FileLock *get_file_lock(const char *file);
