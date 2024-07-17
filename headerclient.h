81091fe176904fe3234bd139a86de09bd6b6546a
oooJordan <158740261+oooJordan@users.noreply.github.com>
2024-07-17 11:39:02 +0200

    Update headerclient.h

--- headerclient.h
+++ headerclient.h
@@ -6,20 +6,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <arpa/inet.h>
 #include <netdb.h>
-#include <linux/limits.h>
-#include <stdbool.h>
 #include <sys/stat.h>
 
 #define BUFFER_SIZE 4096
 
+// prototipi di funzione
 int create_connection(const char *server_address, int server_port);
-void upload_file_on_server(int socket_file_descriptor, char *o_path, char *f_path);
-void download_file_from_server(int socket_file_descriptor, char *o_path, char *f_path);
-FILE* open_file(const char *path, const char *mode);
+void upload_file_on_server(int socket_file_descriptor, char *path_opt_o, char *path_opt_f);
+void download_file_from_server(int socket_file_descriptor, char *path_opt_o, char *path_opt_f);
 void send_command(int socket_file_descriptor, char command, char* path_file_remoto);
-void file_list_on_stout(int socket_file_descriptor, char *f_path);
-void create_dir(const char *path);
+void file_list_on_stout(int socket_file_descriptor, char *path_opt_f);
+void create_dir(const char *percorso_directory);
 
 #endif
